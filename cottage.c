798d348e11648a7db71cc6350c8be3d659f3746a
Harvey Hunt <harveyhuntnexus@gmail.com>
2014-10-21 17:24:34 +0100

    Corrected errors not being sent to stderr.

--- cottage.c
+++ cottage.c
@@ -21,8 +21,8 @@ int main(int argc, char *argv[])
 	int ret, rec;
 
 	if (argc < 2) {
-		printf("usage: cottage <command> [<args>]\n");
-		return EXIT_FAILURE;
+		fprintf(stderr, "usage: cottage <command> [<args>]\n");
+		exit(EXIT_FAILURE);
 	}
 
 	argc--, argv++;
@@ -57,30 +57,30 @@ int main(int argc, char *argv[])
 
 	switch (ret) {
 	case IPC_ERR_SYNTAX:
-		printf("Invalid syntax.\n");
+		fprintf(stderr, "Invalid syntax.\n");
 		break;
 	case IPC_ERR_ALLOC:
-		printf("Couldn't allocate memory to store args.\n");
+		fprintf(stderr, "Couldn't allocate memory to store args.\n");
 		break;
 	case IPC_ERR_NO_CMD:
-		printf("No such command.\n");
+		fprintf(stderr, "No such command.\n");
 		break;
 	case IPC_ERR_TOO_MANY_ARGS:
-		printf("Too many args.\n");
+		fprintf(stderr, "Too many args.\n");
 		break;
 	case IPC_ERR_TOO_FEW_ARGS:
-		printf("Too few args.\n");
+		fprintf(stderr, "Too few args.\n");
 		break;
 	case IPC_ERR_ARG_NOT_INT:
-		printf("Argument wasn't an int\n");
+		fprintf(stderr, "Argument wasn't an int\n");
 		break;
 	case IPC_ERR_ARG_TOO_LARGE:
-		printf("Argument was too large\n");
+		fprintf(stderr, "Argument was too large\n");
 		break;
 	}
 
 	if (close(sock) == -1) {
-		printf("Failed to close socket.\n");
+		perror("Failed to close socket.\n");
 		exit(EXIT_FAILURE);
 	}
 
